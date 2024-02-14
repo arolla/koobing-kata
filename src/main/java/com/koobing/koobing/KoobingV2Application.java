5fc033ea32e0fcbca7c85ff6fae00080460e8585
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-14 04:40:53 +0100

    Init project

---
+++ src/main/java/com/koobing/koobing/KoobingV2Application.java
@@ -0,0 +1,13 @@
+package com.koobing.koobing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoobingV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoobingV2Application.class, args);
+    }
+
+}
