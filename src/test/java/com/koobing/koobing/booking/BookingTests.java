0295023a48173c96108796588ad033e1ed6f391a
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-14 09:14:11 +0100

    write test

---
+++ src/test/java/com/koobing/koobing/booking/BookingTests.java
@@ -0,0 +1,47 @@
+package com.koobing.koobing.booking;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({BookingController.class})
+public class BookingTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Book a room")
+    void bookRoom() throws Exception {
+        var bookingJson = """
+                {
+                    "hostel_id": 1,
+                    "room_id": "46da9f48-ea47-4d9d-9f4b-52b5e56f4e2e",
+                    "arrival": "2020-01-01",
+                    "departure": "2020-01-02",
+                    "email" : "foo.bar@example.com"
+                }
+                """;
+
+        var expectedJson = """
+                {
+                    "booking_number": "A123"
+                }
+                """;
+
+
+        mvc.perform(post("/api/v1/booking")
+                        .contentType("application/json")
+                        .content(bookingJson))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().json(expectedJson));
+
+    }
+}
