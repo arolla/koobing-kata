680951544a4f61d553bfa6ca2e280b249020e162
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-15 19:11:28 +0100

    replace simple test with a parameterized test

--- src/test/java/com/koobing/koobing/booking/BookingTests.java
+++ src/test/java/com/koobing/koobing/booking/BookingTests.java
@@ -2,6 +2,8 @@ package com.koobing.koobing.booking;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -45,18 +47,28 @@ public class BookingTests {
 
     }
 
-    @Test
-    @DisplayName("Try booking a room without email")
-    void bookRoomWithoutEmail() throws Exception {
-        var bookingJson = """
+    @ParameterizedTest
+    @ValueSource(strings = {
+            """
                 {
                     "hostel_id": 1,
                     "room_id": "46da9f48-ea47-4d9d-9f4b-52b5e56f4e2e",
                     "arrival": "2020-01-01",
                     "departure": "2020-01-02"
                 }
-                """;
-
+                    """,
+            """
+                {
+                    "hostel_id": 1,
+                    "room_id": "46da9f48-ea47-4d9d-9f4b-52b5e56f4e2e",
+                    "arrival": "2020-01-01",
+                    "departure": "2020-01-02",
+                    "email" : "foo.bar"
+                }
+                    """
+    })
+    @DisplayName("Try booking a room without email")
+    void bookRoomWithoutEmail(String bookingJson) throws Exception {
         var expectedJson = """
                 {
                     "message": "Invalid email provided."
