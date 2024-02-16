a8940eb359d1cb3b4866dc7b62c5c2ca1556c963
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-16 09:00:00 +0100

    write test which validate the wanted behaviour
    To validate the behaviour, we need to update the search repository stub.

--- src/main/java/com/koobing/koobing/search/repository/InMemoryHotelRepository.java
+++ src/main/java/com/koobing/koobing/search/repository/InMemoryHotelRepository.java
@@ -5,14 +5,19 @@ import com.koobing.koobing.search.domain.Address;
 import com.koobing.koobing.search.domain.Hotel;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public class InMemoryHotelRepository implements HotelRepository {
     @Override
     public List<Hotel> findAvailableHotelsByZipcodeAndDates(String zipcode, LocalDate arrivalDate, LocalDate departureDate) {
-        return List.of(
-                new Hotel(1, "Elegance Hotel", new Address("25 RUE DU LOUVRE", "PARIS", "75001"), 10, 150, List.of("Free Wi-Fi", "Parking", "Complimentary Breakfast")),
-                new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
-        );
+        if (arrivalDate.equals(LocalDate.of(2024, 1, 1))) {
+            return List.of(
+                    new Hotel(1, "Elegance Hotel", new Address("25 RUE DU LOUVRE", "PARIS", "75001"), 10, 150, List.of("Free Wi-Fi", "Parking", "Complimentary Breakfast")),
+                    new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
+            );
+        }
+
+        return Collections.emptyList();
     }
 }
--- src/test/java/com/koobing/koobing/search/SearchServiceTests.java
+++ src/test/java/com/koobing/koobing/search/SearchServiceTests.java
@@ -28,4 +28,19 @@ class SearchServiceTests {
                 new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
         );
     }
+
+    @Test
+    @DisplayName("Search hotel by inverting arrival and departure dates")
+    void searchHotelWithInvertedDates() {
+        HotelRepository hotelRepository = new InMemoryHotelRepository();
+        SearchService searchService = new DefaultSearchService(hotelRepository);
+        List<Hotel> hotels = searchService.availableHostels("75001",
+                LocalDate.of(2024, 1, 2),
+                LocalDate.of(2024, 1, 1));
+
+        assertThat(hotels).containsExactly(
+                new Hotel(1, "Elegance Hotel", new Address("25 RUE DU LOUVRE", "PARIS", "75001"), 10, 150, List.of("Free Wi-Fi", "Parking", "Complimentary Breakfast")),
+                new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
+        );
+    }
 }
