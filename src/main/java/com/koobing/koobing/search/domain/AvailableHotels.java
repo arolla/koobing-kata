88777b93e81f102aaf9949a64684425efe118721
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-16 14:09:44 +0100

    make AvailableHotels as Iterable

--- src/main/java/com/koobing/koobing/search/domain/AvailableHotels.java
+++ src/main/java/com/koobing/koobing/search/domain/AvailableHotels.java
@@ -1,15 +1,26 @@
 package com.koobing.koobing.search.domain;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public record AvailableHotels(List<Hotel> hotels) {
+public record AvailableHotels(List<Hotel> hotels) implements Iterable<Hotel> {
 
     public AvailableHotels(List<Hotel> hotels) {
-        // copyOf -> unmodifiable list
-        this.hotels = List.copyOf(hotels);
+        if (hotels == null) {
+            this.hotels = Collections.emptyList();
+        } else {
+            // copyOf -> unmodifiable list
+            this.hotels = List.copyOf(hotels);
+        }
     }
 
     public boolean isEmpty() {
         return hotels.isEmpty();
+    }
+
+    @Override
+    public Iterator<Hotel> iterator() {
+        return hotels.iterator();
     }
 }
