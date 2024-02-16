e58e102e495531083906687075363cfe665aa94e
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-16 22:37:23 +0100

    add Maven dependency and define Jpa test skeleton

---
+++ src/test/java/com/koobing/koobing/repository/JpaHotelRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.koobing.koobing.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@Sql(scripts = {"/sql/create-tables.sql", "/sql/insert.sql"})
+@Sql(scripts = {"/sql/drop-tables.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+class JpaHotelRepositoryTest {
+
+
+    @Test
+    @DisplayName("search available hostels from DB")
+    void availableHostels() {
+    }
+}
