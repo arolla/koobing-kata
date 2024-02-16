6c3890affff8e9999d58d7da5ff94b32effb8170
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-16 12:26:21 +0100

    write test which manages error or success while hotel search

--- src/test/java/com/koobing/koobing/search/SearchServiceTests.java
+++ src/test/java/com/koobing/koobing/search/SearchServiceTests.java
@@ -4,6 +4,7 @@ import com.koobing.koobing.search.domain.Address;
 import com.koobing.koobing.search.domain.Hotel;
 import com.koobing.koobing.search.repository.InMemoryHotelRepository;
 import com.koobing.koobing.search.service.DefaultSearchService;
+import com.koobing.koobing.utils.Either;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,11 +20,11 @@ class SearchServiceTests {
     void searchHotel() {
         HotelRepository hotelRepository = new InMemoryHotelRepository();
         SearchService searchService = new DefaultSearchService(hotelRepository);
-        List<Hotel> hotels = searchService.availableHostels("75001",
+        Either<SearchError, List<Hotel>> hotels = searchService.availableHostels("75001",
                 LocalDate.of(2024, 1, 1),
                 LocalDate.of(2024, 1, 2));
 
-        assertThat(hotels).containsExactly(
+        assertThat(hotels.right()).containsExactly(
                 new Hotel(1, "Elegance Hotel", new Address("25 RUE DU LOUVRE", "PARIS", "75001"), 10, 150, List.of("Free Wi-Fi", "Parking", "Complimentary Breakfast")),
                 new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
         );
@@ -34,13 +35,26 @@ class SearchServiceTests {
     void searchHotelWithInvertedDates() {
         HotelRepository hotelRepository = new InMemoryHotelRepository();
         SearchService searchService = new DefaultSearchService(hotelRepository);
-        List<Hotel> hotels = searchService.availableHostels("75001",
+        Either<SearchError, List<Hotel>> hotels = searchService.availableHostels("75001",
                 LocalDate.of(2024, 1, 2),
                 LocalDate.of(2024, 1, 1));
 
-        assertThat(hotels).containsExactly(
+        assertThat(hotels.right()).containsExactly(
                 new Hotel(1, "Elegance Hotel", new Address("25 RUE DU LOUVRE", "PARIS", "75001"), 10, 150, List.of("Free Wi-Fi", "Parking", "Complimentary Breakfast")),
                 new Hotel(2, "Charming Inn", new Address("21 RUE DU BOULOI", "PARIS", "75001"), 5, 120, List.of("Free Wi-Fi", "Swimming Pool", "Room Service"))
         );
+    }
+
+    @Test
+    @DisplayName("Search hotel for no night")
+    void searchHotelWithoutOneNightAtLeast() {
+        HotelRepository hotelRepository = new InMemoryHotelRepository();
+        SearchService searchService = new DefaultSearchService(hotelRepository);
+        Either<SearchError, List<Hotel>> hotels = searchService.availableHostels("75001",
+                LocalDate.of(2024, 1, 1),
+                LocalDate.of(2024, 1, 1));
+
+        assertThat(hotels.isLeft()).isTrue();
+        assertThat(hotels.left()).isEqualTo(new SearchError("A booking must contain at least one night."));
     }
 }
