33cffd40b6209079d14494c24074b54ccf789935
Christian Sperandio <christian.sperandio@gmail.com>
2024-02-17 16:19:20 +0100

    write tests about zipcode

--- src/main/java/com/koobing/koobing/search/domain/Zipcode.java
+++ src/main/java/com/koobing/koobing/search/domain/Zipcode.java
@@ -1,7 +1,25 @@
 package com.koobing.koobing.search.domain;
 
-public record Zipcode(String value) {
+import java.util.regex.Pattern;
+
+public class Zipcode {
+    private static final Pattern validZipcodePattern = Pattern.compile("^\\d{5}$");
+    private static final Pattern invalidZipcodePattern = Pattern.compile("(\\d)\\1{4}");
+    private final String value;
+
     public Zipcode(String value) {
+        if (!validZipcodePattern.matcher(value).matches()) {
+            throw new IllegalArgumentException("Invalid zipcode: " + value);
+        }
+
+        if (invalidZipcodePattern.matcher(value).matches()) {
+            throw new IllegalArgumentException("Invalid zipcode: " + value);
+        }
+
         this.value = value;
+    }
+
+    public String value() {
+        return value;
     }
 }
